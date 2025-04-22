8ab647f0c866b618d5733cd0d32698d899152c8b
arimisarn <arimisa.dev@gmail.com>
2025-04-22 18:41:09 +0300

    ajout de cmdk (non terminé)

--- frontend/my-auth-app/src/components/base/CommandMenu.tsx
+++ frontend/my-auth-app/src/components/base/CommandMenu.tsx
@@ -1,5 +1,5 @@
 import { Command } from 'cmdk'
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { Dispatch, SetStateAction, useState } from 'react'
 
 export const CommandMenu = ({
     open, 
@@ -8,6 +8,7 @@ export const CommandMenu = ({
     open: boolean;
     setOpen: Dispatch<SetStateAction<boolean>>;
 }) => {
+    const [value, setValue] = useState("")
 
   // Toggle the menu when ⌘K is pressed
   React.useEffect(() => {
@@ -30,19 +31,29 @@ export const CommandMenu = ({
     className='fixed inset-0 bg-stone-950/50'
     onClick={() => setOpen(false)}
     >
-      <div>
-      <Command.Input />
+      <div 
+      onClick={(e) => e.stopPropagation()}
+      className='dark:bg-zinc-700 bg-white rounded-lg shadow-xl dark:border-stone-800 border-stone-300 border
+      overflow-hidden w-full max-w-lg mx-auto mt-12'
+      >
+      <Command.Input 
+      value={value}
+      onValueChange={setValue}
+      placeholder='Que voulez-vous?'
+      className='dark:text-white relative border-b border-stone-300 p-3 text-md w-full
+      placeholder:text-stone-400 focus:outline-none dark:bg-zinc-700 dark:border-stone-500'
+      />
       <Command.List>
-        <Command.Empty>No results found.</Command.Empty>
+        <Command.Empty className='dark:text-white'>No results found.</Command.Empty>
 
-        <Command.Group heading="Letters">
+        <Command.Group className='dark:text-white' heading="Letters">
           <Command.Item>a</Command.Item>
           <Command.Item>b</Command.Item>
           <Command.Separator />
           <Command.Item>c</Command.Item>
         </Command.Group>
 
-        <Command.Item>Apple</Command.Item>
+        <Command.Item className='dark:text-white'>Apple</Command.Item>
       </Command.List>
       </div>
     </Command.Dialog>
