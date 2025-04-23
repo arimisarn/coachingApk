758d673fadfd61e44aee195c65dd6809494f2452
arimisarn <arimisa.dev@gmail.com>
2025-04-23 12:27:10 +0300

    auth detail

--- backend/accounts/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import CustomUser
-
-class CustomUserCreationForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'password', 'bio', 'profile_picture', 'coaching_type']
-
-    password = forms.CharField(widget=forms.PasswordInput)
